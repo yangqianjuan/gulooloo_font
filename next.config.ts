bcccbf249fdcb4b4cad43a0a4dabac4fc6139423
kateyang <1072483500@qq.com>
2025-08-08 12:03:21 +0800

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
-const withSitemap = require("next-sitemap");
 
 const nextConfig: NextConfig = {
-  ...withSitemap,
   //  output: "export"
 };
 
