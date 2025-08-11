4b3b0b85295ce71815a2a4c858d7c2edf51e6318
kateyang <1072483500@qq.com>
2025-08-11 17:10:55 +0800

    add policy,service

---
+++ src/app/policy/page.tsx
@@ -0,0 +1,4 @@
+// src/app/policy.tsx
+export default function Policy() {
+  return <div>这是隐私政策页面</div>;
+}
---
+++ src/app/service/page.tsx
@@ -0,0 +1,4 @@
+// src/app/service.tsx
+export default function Service() {
+  return <div>这是服务页面</div>;
+}
