8abb87283595dec1b42af08c2931f71b83487295
kateyang <1072483500@qq.com>
2025-08-12 16:36:04 +0800

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,30 @@
 // 字体加载
 // import "./globals.css";
 import { Poppins } from "next/font/google";
+import type { Metadata } from "next";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "600"],
   display: "swap",
 });
+
+// ✅ 动态设置 Metadata
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+
+  // 导入对应语言的 messages
+  const messages = await import(`@/messages/${locale}.json`).then(
+    (m) => m.default
+  );
+  return {
+    title: messages.home?.metaTitle || "Default Title",
+    description: messages.home?.metaDescription || "Default description.",
+  };
+}
 
 export default function RootLayout({
   children,
@@ -15,6 +34,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={poppins.className}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>{children}</body>
     </html>
   );
