1013c40b5d9e65155d870048256b5a98fdfe7394
kateyang <1072483500@qq.com>
2025-08-20 11:22:54 +0800

    add asset

---
+++ src/app/[locale]/blog/Banner.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import banner from "@/assets/blog_banner/blog_banner_Q60.webp";
+import m_banner from "@/assets/blog_banner/blog_banner_Q40.webp";
+import { useTranslations } from "next-intl";
+
+export default function Banner() {
+  const t = useTranslations("home");
+  return (
+    <div className="relative">
+      <div className="absolute top-1/2  -translate-y-1/2 text-center w-full text-white font-bold  xl:text-[4.5rem] sm:text-[2.75rem] text-[1.5rem]">
+        Insights for a Better Everyday
+      </div>
+      <Image
+        src={banner}
+        alt={t("top_banner_alt")}
+        className="w-full h-auto hidden sm:block"
+        priority
+        width={3840}
+        height={1360}
+        sizes="(max-width: 768px) 100vw, 3840px"
+        fetchPriority="high"
+        placeholder="blur"
+        // unoptimized
+      ></Image>
+      <Image
+        src={m_banner}
+        alt={t("top_banner_mobile_alt")}
+        className="w-full h-auto block sm:hidden"
+        width={3840}
+        height={1360}
+        priority
+        fetchPriority="high"
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, 1170px"
+        // unoptimized
+      ></Image>
+    </div>
+  );
+}
--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+import Header from "@/components/Header";
+import dynamic_ from "next/dynamic";
+import Banner from "./Banner";
+const Footer = dynamic_(() => import("@/components/Footer"), { ssr: false });
 export default function BlogPage() {
-  return <div>blog</div>;
+  return (
+    <div className="grid grid-rows-[auto_1fr_auto]  justify-items-center min-h-screen max-w-[1920px] mx-auto sm:min-w-[1080px]">
+      <Header />
+      <main className="flex flex-col  row-start-2 sm:items-start w-full">
+        <Banner />
+      </main>
+      <Footer />
+    </div>
+  );
 }
