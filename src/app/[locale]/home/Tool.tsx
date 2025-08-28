17ffd4abb1909e411b9278ac713344d29c22bf44
kateyang <1072483500@qq.com>
2025-08-28 11:16:00 +0800

    update tool

--- src/app/[locale]/home/Tool.tsx
+++ src/app/[locale]/home/Tool.tsx
@@ -112,75 +112,61 @@ export default function Tool() {
             } sm:justify-between text-left flex-col items-center  sm:mb-[7.5rem] 2xl:px-[7.5rem] sm:px-[3.75rem] px-[3.15rem]  mb-[6rem]`}
             key={d.textInfo.title}
           >
-            <div>
+            <div className="2xl:w-[42%] sm:w-[44%] w-full">
               <Image
                 src={d.img}
-                className="2xl:w-[43.25rem] sm:w-[26.875rem] w-full sm:mb-[0rem] mb-[2.5rem]"
+                className="w-full sm:mb-[0rem] mb-[2.5rem]"
                 alt=""
                 loading="lazy"
                 width={1384}
                 height={1560}
-                sizes="(max-width: 640px) 100vw, (max-width: 1280px) 26.875, 43.25rem"
+                sizes="(max-width: 640px) 100vw, (max-width: 1280px) 26.875rem, 43.25rem"
               />
             </div>
-            <div className="2xl:w-[50rem] 2xl:pt-[7.37rem]  sm:w-[28.75rem] sm:pt-[2.5rem]  w-full sm:px-[0rem] px-[2.5rem]">
-              <div className="font-semibold 2xl:text-[4rem] sm:text-[2.5rem] 2xl:mb-[1.5rem] sm:mb-[1rem] mb-[0.5rem] text-[1.375rem]">
-                {d.textInfo.title}
-              </div>
-              <div className="text-[rgba(4,30,84,1)] 2xl:text-[1.5rem] sm:text-[1.25rem] axl:mb-[2.5rem]  sm:mb-[1.6rem] mb-[1rem] text-[0.9735rem]">
-                {d.textInfo.desc}
-              </div>
-              <ul className="sm:mb-[8.75rem] mb-[4rem]">
-                {d.textInfo.items.map((item) => {
-                  return (
-                    <li
-                      className="flex items-center w-full mb-[1rem]"
-                      key={item}
-                    >
-                      <Image
-                        src={ic}
-                        alt=""
-                        className="w-[1.5rem] mr-[1.6rem]"
-                        loading="lazy"
-                      />
-                      <div className="2xl:text-[1.5rem] sm:text-[1.25rem] text-[0.9375rem] ">
-                        {item}
-                      </div>
-                    </li>
-                  );
-                })}
-              </ul>
-              <div className="flex">
-                <span
-                  onClick={() => {
-                    toLink(d.link_ios);
-                  }}
-                >
-                  <AppStore className="text-[rgba(0,0,0,1)] hover:text-[rgba(4,30,84,1)] 2xl:w-[10.125rem] 2xl:mr-[1.5rem] w-[8.4375rem] mr-[1.25rem] transition-transform duration-300 hover:scale-110" />
-                </span>
-                <span
-                  onClick={() => {
-                    toLink(d.link_android);
-                  }}
-                >
-                  <GooglePlay className="text-[rgba(0,0,0,1)] hover:text-[rgba(4,30,84,1)] 2xl:w-[10.125rem] 2xl:mr-[1.5rem] w-[8.4375rem] mr-[1.25rem] transition-transform duration-300 hover:scale-110" />
-                </span>
-                {/* <Image
-                  src={Appstore_download}
-                  alt={t("appstore_download_alt")}
-                  onClick={() => {
-                    toLink(d.link_ios);
-                  }}
-                  className="2xl:w-[10.125rem] 2xl:mr-[1.5rem] w-[8.4375rem] mr-[1.25rem] transition-transform duration-300 hover:scale-110"
-                /> */}
-                {/* <Image
-                  src={googleplay_download}
-                  alt={t("googleplay_download_alt")}
-                  onClick={() => {
-                    toLink(d.link_android);
-                  }}
-                  className="2xl:w-[10.125rem] w-[8.4375rem] transition-transform duration-300 hover:scale-110"
-                /> */}
+            <div className="2xl:w-[47%]  sm:w-[48%]  w-full sm:px-[0rem] px-[2.5rem] flex items-center">
+              <div>
+                <div className="font-semibold 2xl:text-[4rem] sm:text-[2.5rem] 2xl:mb-[1.5rem] sm:mb-[1rem] mb-[0.5rem] text-[1.375rem]">
+                  {d.textInfo.title}
+                </div>
+                <div className="text-[rgba(4,30,84,1)] 2xl:text-[1.5rem] sm:text-[1.25rem] 2xl:mb-[2.5rem]  sm:mb-[1.6rem] mb-[1rem] text-[0.9735rem]">
+                  {d.textInfo.desc}
+                </div>
+                <ul className="2xl:mb-[8.75rem] sm:mb-[5rem] mb-[4rem]">
+                  {d.textInfo.items.map((item) => {
+                    return (
+                      <li
+                        className="flex items-center w-full mb-[1rem]"
+                        key={item}
+                      >
+                        <Image
+                          src={ic}
+                          alt=""
+                          className="w-[1.5rem] mr-[1.6rem]"
+                          loading="lazy"
+                        />
+                        <div className="2xl:text-[1.5rem] sm:text-[1.25rem] text-[0.9375rem] ">
+                          {item}
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+                <div className="flex">
+                  <span
+                    onClick={() => {
+                      toLink(d.link_ios);
+                    }}
+                  >
+                    <AppStore className="text-[rgba(0,0,0,1)] hover:text-[rgba(4,30,84,1)] 2xl:w-[10.125rem] 2xl:mr-[1.5rem] w-[8.4375rem] mr-[1.25rem] transition-transform duration-300 hover:scale-110" />
+                  </span>
+                  <span
+                    onClick={() => {
+                      toLink(d.link_android);
+                    }}
+                  >
+                    <GooglePlay className="text-[rgba(0,0,0,1)] hover:text-[rgba(4,30,84,1)] 2xl:w-[10.125rem] 2xl:mr-[1.5rem] w-[8.4375rem] mr-[1.25rem] transition-transform duration-300 hover:scale-110" />
+                  </span>
+                </div>
               </div>
             </div>
           </div>
