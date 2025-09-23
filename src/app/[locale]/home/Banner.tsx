21aecb9fe5835071e1d43724a793f37056b8f473
kateyang <1072483500@qq.com>
2025-09-23 15:07:14 +0800

    update

--- src/app/[locale]/home/Banner.tsx
+++ src/app/[locale]/home/Banner.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import banner from "@/assets/image/top_banner/top_banner_2x1.webp";
+import banner from "@/assets/image/top_banner/top_banner_2x.webp";
 import m_banner from "@/assets/image/top_banner_moblie/top_banner_moblie_3x1.webp";
 import { useTranslations } from "next-intl";
 
