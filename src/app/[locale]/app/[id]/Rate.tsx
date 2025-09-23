e08e4b4372f5c71819f1e5eb87e0edd54e079b75
kateyang <1072483500@qq.com>
2025-09-23 11:37:17 +0800

    update

--- src/app/[locale]/app/[id]/Rate.tsx
+++ src/app/[locale]/app/[id]/Rate.tsx
@@ -89,7 +89,7 @@ export default function Rate() {
                     width={32}
                     src={d.user_img}
                     alt={d.user_name}
-                    className="2xl:mr-[13px] mr-[4px] 2xl:w-[40px] w-[32px]"
+                    className="2xl:mr-[12px] mr-[8px] 2xl:w-[40px] w-[32px]"
                   ></Image>
                   <span>{d.user_name}</span>
                 </div>
